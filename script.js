dbfc99cfb87f814de9cce9ecca28bc4593b46c25
Zachary <zacharylampi96@gmail.com>
2022-05-26 14:09:16 -0400

    changed background color script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "My life so far:";
 
 document.querySelector("body").appendChild(h2);
